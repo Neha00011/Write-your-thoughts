3a88d5052acb3b5702c1108e63aedaf92bf86da9
Neha Panda <112271756+Neha00011@users.noreply.github.com>
2024-11-03 21:37:04 +0000

    adding some UI

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -4,16 +4,18 @@ import { Link } from "react-router-dom";
 
 function PostCard({ $id, title, featuredImage }) {
   return (
-    <Link to={`/post/${$id}`}>
-      <div className="w-full bg-gray-100 rounded-xl p-4">
-        <div className="w-full justify-center mb-4">
+    <Link to={`/post/${$id}`} className="w-full">
+      <div className="bg-gradient-to-br from-gray-700 to-gray-800 rounded-xl p-4 shadow-lg hover:shadow-2xl transition duration-200 transform hover:scale-105">
+        <div className="w-full flex justify-center mb-4">
           <img
             src={appwriteService.getFilePreview(featuredImage)}
             alt={title}
-            className="rounded-xl"
+            className="rounded-xl object-cover w-full h-48"
           />
         </div>
-        <h2 className="text-xl font-bold">{title}</h2>
+        <h2 className="text-lg font-semibold text-white text-center">
+          {title}
+        </h2>
       </div>
     </Link>
   );
